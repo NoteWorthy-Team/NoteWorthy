c16de53364fb5c501afd9b5e3690b0e195d60137
wendy <huawendy8@yahoo.ca>
2020-02-19 00:30:23 -0500

    initial push for login page

---
+++ LoginForm.js
@@ -0,0 +1,34 @@
+class LoginForm extends HTMLElement {
+    constructor() {
+      super()
+      this.renderElement = this.renderElement.bind(this)
+      this.renderElement()
+    }
+
+    renderElement() {
+      const login = document.createElement('div')
+      login.className = 'login-container'
+
+      const username = document.createElement('input')
+      username.name = 'username'
+      username.type = 'text'
+      username.placeholder = 'Username'
+
+      const password = document.createElement('input')
+      password.name = 'password'
+      password.type = 'password'
+      password.placeholder = 'Password'
+
+      const submit = document.createElement('input')
+      submit.value = 'Sign In'
+      submit.type = 'submit'
+
+      login.appendChild(username)
+      login.appendChild(password)
+      login.appendChild(submit)
+
+      document.body.appendChild(login)
+    }
+  }
+  
+  customElements.define('login-form', LoginForm);
