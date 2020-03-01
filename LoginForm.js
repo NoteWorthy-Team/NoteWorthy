337db1de6ebe28b0e7cc15839d7becc2ab129308
wendy <huawendy8@yahoo.ca>
2020-02-29 20:58:46 -0500

    take out code for custom element of login form

--- LoginForm.js
+++ LoginForm.js
@@ -1,65 +1,29 @@
-class LoginForm extends HTMLElement {
-    constructor() {
-      super()
-      this.renderElement = this.renderElement.bind(this)
-      this.renderElement()
-    }
+const loginForm = document.getElementsByClassName("login-form")[0];
 
-    renderElement() {
-      const login = document.createElement('div')
-      login.className = 'login-form'
+loginForm.addEventListener('submit', loginAttempt);
 
-      const username = document.createElement('input')
-      username.name = 'username'
-      username.type = 'text'
-      username.placeholder = 'Username'
+function loginAttempt(e) {
+  e.preventDefault(); // prevent default form action
 
-      const password = document.createElement('input')
-      password.name = 'password'
-      password.type = 'password'
-      password.placeholder = 'Password'
+  const submittedUsername = e.srcElement.elements.username.value;
+  const submittedPassword = e.srcElement.elements.password.value;
 
-      const submit = document.createElement('input')
-      submit.value = 'Sign In'
-      submit.type = 'submit'
-
-      login.appendChild(username)
-      login.appendChild(document.createElement('br'))
-      login.appendChild(password)
-      login.appendChild(document.createElement('br'))
-      login.appendChild(submit)
-
-      document.body.appendChild(login)
-    }
+  if( submittedUsername == 'user' && submittedPassword =='user')
+  {
+    window.location.href = './users/user_0.html';
   }
 
-  customElements.define('login-form', LoginForm);
-  const loginForm = document.getElementsByClassName("login-form")[0];
-
-  loginForm.addEventListener('submit', loginAttempt);
-
-  function loginAttempt(e) {
-  	e.preventDefault(); // prevent default form action
-
-    const submittedUsername = e.srcElement.elements.username.value;
-    const submittedPassword = e.srcElement.elements.password.value;
-
-    if( submittedUsername == 'user' && submittedPassword =='user')
-    {
-      window.location.href = './users/user_0.html';
-    }
-
-    if( submittedUsername == 'user2' && submittedPassword =='user2')
-    {
-      window.location.href = './users/user_1.html';
-    }
-
-    if( submittedUsername == 'user3' && submittedPassword =='user3')
-    {
-      window.location.href = './users/user_1.html';
-    }
-    if(submittedUsername == 'admin' && submittedPassword == 'admin')
-    {
-      window.location.href = './admin/admin-dashboard.html';
-    }
+  if( submittedUsername == 'user2' && submittedPassword =='user2')
+  {
+    window.location.href = './users/user_1.html';
   }
+
+  if( submittedUsername == 'user3' && submittedPassword =='user3')
+  {
+    window.location.href = './users/user_1.html';
+  }
+  if(submittedUsername == 'admin' && submittedPassword == 'admin')
+  {
+    window.location.href = './admin/admin-dashboard.html';
+  }
+}
