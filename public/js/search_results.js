851b2e8b98f63b88eedfed1fa52130e9b64cc09d
Hannah Keating <34624322+RandamQueen@users.noreply.github.com>
2020-03-30 10:10:59 +0100

    Update search_results.js

--- public/js/search_results.js
+++ public/js/search_results.js
@@ -13,64 +13,75 @@ class albumResult {
     }
 }
 
-const hammer = new albumResult(0, "Please Hammer Don't Hurt 'Em", './img/please_hammer_dont_hurt_em.jpg',
-    'MC Hammer', '1990', 'albums/album_0.html')
-const bahen = new albumResult(1, 'Bahen...', './img//bahen....jpg',
-    'NoteWorthy', '2020', 'albums/album_1.html')
-const help = new albumResult(2, 'Help!', './img//help.jpg',
-    'The Beatles', '1965', 'albums/album_2.html')
-
-albums.push(hammer)
-albums.push(bahen)
-albums.push(help)
-
 // Isolating the search result string from the URL
 const searchArray = window.location.search.split('=')
 
-function displayResults() {
+// Returns a parsed JSON of all the albums in the database.
+function getAlbums() {
+    // the URL for the request
+    const url = '/albums';
 
-    const placement = document.querySelector('#results')
+    // Since this is a GET request, simply call fetch on the URL
+    fetch(url)
+    .then((res) => { 
+        if (res.status === 200) {
+            // return a promise that resolves with the JSON body
+           return res.json() 
+       } else {
+            alert('Could not get the albums')
+       }                
+    })
+    .then((json) => {  // the resolved promise with the JSON body
+        //const parsedAlbums = JSON.parse(json)
+    
+        // The actual string being queried
+        const searchQuery = searchArray[searchArray.length - 1]
+    
+        const theseAlbums = json.albums.filter((album) => album.name.search(searchQuery) !== -1)
+        console.log(theseAlbums)
+    }).catch((error) => {
+        console.log(error)
+    })
+}
+
+/* function displayResults() {
+    //const placement = document.querySelector('#results')
+    const parsedAlbums = JSON.parse(getAlbums())
+    console.log(parsedAlbums)
+    // The actual string being queried
     const searchQuery = searchArray[searchArray.length - 1]
-
-    for (let i = 0; i < albums.length; i++) {
+    const theseAlbums = parsedAlbums.filter((album) => album.title.search(searchQuery) !== -1)
+    console.log(theseAlbums)
+ */
+/*     for (let i = 0; i < albums.length; i++) {
         if (albums[i].albumName.toLowerCase().search(searchQuery) !== -1) {
-
             const albumCoverLink = document.createElement('a')
             albumCoverLink.href = albums[i].link
-
             const albumInfoLink = document.createElement('a')
             albumInfoLink.href = albums[i].link
-
             const albumResult = document.createElement('div')
             albumResult.className = 'albumResult'
-
             const resultAlbumCover = document.createElement('img')
             resultAlbumCover.className = 'albumCover'
             resultAlbumCover.src = albums[i].albumCover
-
             const albumInfo = document.createElement('div')
             albumInfo.className = 'albumInfo'
-
             const resultArtistName = document.createElement('h2')
             resultArtistName.innerText = albums[i].artist
             const resultAlbumName = document.createElement('h2')
             resultAlbumName.innerText = albums[i].albumName
             const resultYear = document.createElement('h2')
             resultYear.innerText = albums[i].year
-
             albumInfo.appendChild(resultArtistName)
             albumInfo.appendChild(resultAlbumName)
             albumInfo.appendChild(resultYear)
-
             albumCoverLink.appendChild(resultAlbumCover)
             albumInfoLink.appendChild(albumInfo)
-
             albumResult.appendChild(albumCoverLink)
             albumResult.appendChild(albumInfoLink)
-
             placement.appendChild(albumResult)
         }
-    }
-}
+    } */
+/* } */
 
-displayResults()
+getAlbums()
